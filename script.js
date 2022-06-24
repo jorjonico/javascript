649e8f499767204145f8e3f38a53c660c3499eca
Jorge Matinez <jorjonicodcv@gmail.com>
2022-06-24 11:19:30 -0300

    El cuento completo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let entrada = prompt("¿Queres que te cuente el cuento de la buena pipa?").toLowerCase()
 
 while(entrada != "no quiero"){
-    alert("Yo no te dije " + entrada + ", ¿NO QUIERES?");
-    entrada = prompt("Te dije si... ¿queres que te cuente el cuento de la buena pipa?");
+    alert("¿NO QUIERES? Yo no te dije " + entrada);
+    entrada = prompt("Te dije si... ¿queres que te cuente el cuento de la buena pipa?").toLowerCase();
 } alert("perfecto!!!, por que no lo sabía")
