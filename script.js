6c678fb3bd268ea8fce7db3d3afbd421c6365e05
Jorge Matinez <jorjonicodcv@gmail.com>
2022-06-24 10:42:24 -0300

    cuento de la buena pipa

---
+++ script.js
@@ -0,0 +1,6 @@
+let entrada = prompt("¿Queres que te cuente el cuento de la buena pipa?").toLowerCase()
+
+while(entrada != "no quiero"){
+    alert("Yo no te dije " + entrada + ", ¿NO QUIERES?");
+    entrada = prompt("Te dije si... ¿queres que te cuente el cuento de la buena pipa?");
+} alert("perfecto!!!, por que no lo sabía")
